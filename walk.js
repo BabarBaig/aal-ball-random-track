f135255943b9e618d316455e1c4c6c242e71c02e
Babar Baig <BobBaig@outlook.com>
2021-10-10 16:02:23 -0400

    Restore comments

--- walk.js
+++ walk.js
@@ -32,29 +32,46 @@ function changeDirectionIfNecessary(ball, x, y) {
   }
 }
 
+// TODO: implement the create function
 function create(color, dx, dy) {
   const newBall = Object.create(this);
+
+  // TODO: Set newBall properties: dx, dy, width, height
   newBall.dx = dx;
   newBall.dy = dy;
   newBall.width = dx;
   newBall.height = dy;
+
+  // TODO: set the newBall.element property and initialize it to a Html element "div"
   newBall.element = document.createElement('div');
+
+  // TODO: set the backgroundColor, width and height style properties for newBall.element
   newBall.element.style.backgroundColor = color;
   newBall.element.style.width = newBall.width + 'px';
   newBall.element.style.height = newBall.height + 'px';
+
+  // This line set the CSS class for newBall.element. No need to change this line
   newBall.element.className = 'ball';
+
+  // TODO: set the width and height of newBall based on newBall.element
+  // Hint: use the Javascript parseInt() function to convert a string value to integer
   newBall.width = parseInt(newBall.element.style.width);
   newBall.height = parseInt(newBall.element.style.height);
+
+  // TODO: use the Javascript appendChild() function to add newBall.element to the area element
   area.element.appendChild(newBall.element);
+
   return newBall;
 }
 
 // TODO: implement the update function
 function update(ball, x, y) {
   // TODO: use the moveTo() function to move the ball
-  // TODO: use the Javascript setTimeout() method to call changeDirectionIfNecessary() and update() every 16ms
-  moveTo(ball, x, y)
-  changeDirectionIfNecessary(ball, x, y)
+  // TODO: use the Javascript setTimeout() method to call changeDirectionIfNecessary() and update()
+  //       every 16ms
+  // moveTo(ball, x, y)
+  // changeDirectionIfNecessary(ball, x, y)
+  // setTimeout(update, 16)
 }
 
 // Uncomment these lines for step 1 of the activity
@@ -68,12 +85,14 @@ moveTo(ball1, 1, 1);
 moveTo(ball2, 10, 10);
 moveTo(ball3, 20, 20);
 
+var elm = document.getElementsByClassName("ball");
+console.log(elm.length)
+
 // Uncomment these lines for step 2 of the activity
 // This is expected to make the 3 balls move around the area div
-
-update(ball1, 70, 0);
-update(ball2, 20, 200);
-update(ball3, 300, 330);
+// update(ball1, 70, 0);
+// update(ball2, 20, 200);
+// update(ball3, 300, 330);
 
 // Do not change code past this point
 if (typeof module !== 'undefined') {
